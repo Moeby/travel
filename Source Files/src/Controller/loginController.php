b357d5787dfd3340e8f63d83422e30ddb6b2dae0
matalie <natalie.stalder1@gmail.com>
2017-09-27 11:22:33 +0200

    loginController 2

---
+++ Source Files/src/Controller/loginController.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: natal
+ * Date: 27.09.2017
+ * Time: 09:32
+ */
+
+
+namespace Controller;
+
+
+class LoginController {
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function checkLoginAction() {
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction() {
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAcn(AuthenticationUtils $authUtils) {
+        $error = $authUtils->getLastAuthenticationError();
+        $lastUsername = $authUtils->getLastUsername();
+
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->setAction($this->generateUrl('login_check'))
+            ->add('username', TextType::class, array('property_path' => '_username'))
+            ->add('password', PasswordType::class, array('property_path' => '_password'))
+            ->add('save', SubmitType::class, array('label' => 'Login'))
+            ->getForm()
+        ;
+
+        return $this->render('login.html.twig', [
+            'title' => "Login",
+            'form' => $form->createView(),
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
+    }
+
+    public function loginAction($user, $password){
+
+    }
+
+    function checkPassword($password, $user){
+        $compPassword = $user.get_password();
+        $pepper       = "NatAnA";
+        $salt         = $user.get_salt();
+        $saltpepper   = $salt + $pepper;
+        $options      = [
+            'salt' => $saltpepper
+            //    $salt = uniqid(mt_rand(), true);
+        ];
+        if($compPassword === password_hash($password, PASSWORD_BCRYPT, $options)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+}
--- Source Files/src/Controller/signUp.php
+++ Source Files/src/Controller/signUp.php
@@ -3,5 +3,5 @@
  * Created by PhpStorm.
  * User: natal
  * Date: 27.09.2017
- * Time: 09:32
+ * Time: 10:31
  */
