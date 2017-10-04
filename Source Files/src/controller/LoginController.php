9fa637f940733916966dfa001a636e74d74bdb12
matalie <natalie.stalder1@gmail.com>
2017-10-04 10:03:54 +0200

    slim Controllers

--- Source Files/src/controller/LoginController.php
+++ Source Files/src/controller/LoginController.php
@@ -3,51 +3,23 @@
 class LoginController extends Controller {
 
     /**
-     * @Route("/login_check", name="login_check")
-     */
-    public function checkLoginAction() {
-    }
-
-    /**
      * @Route("/logout", name="logout")
      */
     public function logoutAction() {
     }
 
-    /**
-     * @Route("/login", name="login")
-     */
-    public function loginAcn(AuthenticationUtils $authUtils) {
-        $error = $authUtils->getLastAuthenticationError();
-        $lastUsername = $authUtils->getLastUsername();
-
-        $user = new User();
-        $form = $this->createFormBuilder($user)
-            ->setAction($this->generateUrl('login_check'))
-            ->add('username', TextType::class, array('property_path' => '_username'))
-            ->add('password', PasswordType::class, array('property_path' => '_password'))
-            ->add('save', SubmitType::class, array('label' => 'Login'))
-            ->getForm()
-        ;
-
-        return $this->render('login.html.twig', [
-            'title' => "Login",
-            'form' => $form->createView(),
-            'error' => $error,
-            'last_username' => $lastUsername
-        ]);
-    }
 
     public function configureOptions(OptionsResolver $resolver) {
-        $resolver->setDefaults(array(
-            'data_class' => User::class,
-        ));
+
     }
 
     public function loginAction($user, $password){
 
     }
 
+    /**
+     * @Route("/login_check", name="login_check")
+     */
     function checkPassword($password, $user){
         $compPassword = $user.get_password();
         $pepper       = "NatAnA";
--- Source Files/src/controller/SignUpController.php
+++ Source Files/src/controller/SignUpController.php
@@ -6,44 +6,7 @@ class SignupController extends Controller {
      * @Route("/signup", name="signup")
      */
     public function signupAction(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
-        $user = new User();
-        $error = "";
-        $form = $this->createFormBuilder($user)
-            ->setMethod('POST')
-            ->add('username', TextType::class)
-            ->add('password', RepeatedType::class, array(
-                'type' => PasswordType::class,
-                'first_options' => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat password')))
-            ->add('save', SubmitType::class, array('label' => 'Signup'))
-            ->getForm()
-        ;
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->get('doctrine')->getManager();
-            $user = $form->getData();
-            // check if username is already taken
-            $query = $em->createQuery('SELECT u FROM AppBundle:User u WHERE u.username = ?1');
-            $query->setParameter(1, $user->getUsername());
-            $results = $query->getResult();
-
-            if (empty($results)) {
-                $password = $passwordEncoder->encodePassword($user, $user->getPassword());
-                $user->setPassword($password);
-                $user->setBlog($this->createBlogAction());
-                $em->persist($user);
-                $em->flush();
-
-                return $this->redirectToRoute('login');
-            }
-            $error = "Username already taken";
-        }
-        return $this->render('signup.html.twig', [
-            'title' => "Sign Up",
-            'form' => $form->createView(),
-            'error' => $error
-        ]);
     }
 
     function setSaltedHash($password) {
