8c4ca8ca0fc706d2821b0a570a86a11ecfc4253a
teshi24 <nadja.stadi@gmail.com>
2017-10-24 17:17:17 +0200

    view add post added

---
+++ Source Files/src/controller/PostController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nadja
+ * Date: 24.10.2017
+ * Time: 17:00
+ */
+
+namespace src\controller;
+
+
+class PostController
+{
+    public function addPost(){
+
+    }
+}
