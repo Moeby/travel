0ec58fd364ea4c4cc29e3dea8728799f444bf713
matalie <natalie.stalder1@gmail.com>
2017-10-25 11:16:49 +0200

    Map

--- Source Files/src/controller/LoginController.php
+++ Source Files/src/controller/LoginController.php
@@ -1,9 +1,14 @@
 <?php
 namespace  Travel\Controller;
 
+use Doctrine\DBAL\Platforms\Keywords\ReservedKeywordsValidator;
+
 class LoginController extends Controller {
 
+    public $salt;
+
     public function logoutAction() {
+
     }
 
 
@@ -13,16 +18,34 @@ class LoginController extends Controller {
 
     /*http://localhost/travel/travel/Source%20Files/src/index.php?controller=Login&action=loginAction*/
     public function loginAction($html){   
-        $content = file_get_contents(ROOTPATH."Source Files/app/resources/view/login.html");
+        $content = file_get_contents(RESOURCE_ROOT."view/login.html");
         $content = str_replace("{{rootpath}}", ROOTPATH, $content);
-        $htmlLogin = str_replace("{{pageContent}}", $content, $html);
+        $html    = str_replace("{{pageContent}}", $content, $html);
 
-        return $htmlLogin;
+        return $html;
+    }
+
+    public function checkUserAction(){
+        if (!empty($_POST)){
+            $username      = $_POST["username"];
+            $givenPassword = $_POST["password"];
+
+            $criteria     = array('username'=>$username);
+            $em           = $this->getEntityManager();
+            $user         = $em->getRepository('Travel\Entity\User')->findBy($criteria);
+            $password     = $user->getPassword();
+            salt          = $user->getSalt();
+
+            $compPassword = $this->getSaltedHash($givenPassword);
+        }else{
+            echo "Empty post request";
+        }
+        exit;//redirect here
     }
 
     function checkPassword(){
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+
+
         //get user from db with username
         $compPassword = $user.get_password();
         $pepper       = "NatAnA";
@@ -37,6 +60,15 @@ class LoginController extends Controller {
         } else {
             return false;
         }
+    }
+
+    function getSaltedHash($password)
+    {
+        $this->salt = $this->genSalt();
+        $options = [
+            'salt' => $this->salt
+        ];
+        return password_hash($password . $this->pepper, PASSWORD_BCRYPT, $options);
     }
 
 }
--- Source Files/src/controller/MapController.php
+++ Source Files/src/controller/MapController.php
@@ -1,15 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: natal
- * Date: 25.10.2017
- * Time: 08:24
- */
-
-namespace src\controller;
-
-
-class MapController
+require_once $_SERVER["DOCUMENT_ROOT"] . "/travel/travel/Source Files/app/config/dbConfig.php";
+class MapController extends Controller
 {
 
+    public function mapAction($html){
+        $content = file_get_contents(RESOURCE_ROOT.'view/map.html');
+    }
+
+    public function getAllPostItems(){
+        $em = $this->getEntityManager();
+        //TODO: get all location_user with certain user, get posts to this location_user and fill in array [latitude, longitude, postname]
+    }
 }
